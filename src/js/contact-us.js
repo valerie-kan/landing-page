0eeba7ebe234b6b65e761a483274efec2f62588a
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2025-05-08 14:52:51 +0300

    add form validation

---
+++ src/js/contact-us.js
@@ -0,0 +1,100 @@
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const textarea = document.getElementById('comment');
+const button = document.getElementById('formBtn');
+const form = document.getElementById('contactForm');
+
+let nameTouched = false;
+let emailTouched = false;
+let commentTouched = false;
+
+const isValid = () => {
+  const isFormValid =
+    nameInput.value.trim() !== '' &&
+    emailInput.value.trim() !== '' &&
+    textarea.value.trim() !== '';
+
+  button.disabled = !isFormValid;
+
+  button.classList.toggle('disabled', !isFormValid);
+};
+
+const validateName = () => {
+  const error = document.getElementById('errName');
+  const value = nameInput.value.trim();
+
+  if (nameTouched && value === '') {
+    nameInput.classList.add('invalid');
+    error.style.display = 'block';
+  } else {
+    nameInput.classList.remove('invalid');
+    nameInput.classList.add('valid');
+    error.style.display = 'none';
+  }
+
+  isValid();
+};
+
+const validateEmail = () => {
+  const error = document.getElementById('errEmail');
+  const value = emailInput.value.trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailTouched && (value === '' || !emailRegex.test(value))) {
+    emailInput.classList.add('invalid');
+    emailInput.classList.remove('valid');
+    error.style.display = 'block';
+  } else {
+    emailInput.classList.remove('invalid');
+    emailInput.classList.add('valid');
+    error.style.display = 'none';
+  }
+
+  isValid();
+};
+
+const validateComment = () => {
+  const error = document.getElementById('errComment');
+  const value = textarea.value;
+
+  if (commentTouched && value === '') {
+    textarea.classList.add('invalid');
+    error.style.display = 'block';
+  } else {
+    textarea.classList.remove('invalid');
+    textarea.classList.add('valid');
+    error.style.display = 'none';
+  }
+
+  isValid();
+};
+
+nameInput.addEventListener('blur', () => {
+  nameTouched = true;
+  validateName();
+});
+
+nameInput.addEventListener('input', validateName);
+
+emailInput.addEventListener('blur', () => {
+  emailTouched = true;
+  validateEmail();
+});
+
+emailInput.addEventListener('input', validateEmail);
+
+textarea.addEventListener('blur', () => {
+  commentTouched = true;
+  validateComment();
+});
+
+textarea.addEventListener('input', validateComment);
+
+form.addEventListener('submit', e => {
+  isValid();
+  e.preventDefault();
+  form.reset();
+  nameInput.classList.remove('valid');
+  emailInput.classList.remove('valid');
+  textarea.classList.remove('valid');
+});
---
+++ src/main.js
@@ -0,0 +1,2 @@
+import * as contactUs from './js/contact-us';
+import * as faq from './js/faq';
