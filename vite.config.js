a607c01d6b273ae397900fad8283193194a5acd9
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2025-05-08 21:06:27 +0300

    change settings

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: '/landing-page',
     build: {
       sourcemap: true,
       rollupOptions: {
